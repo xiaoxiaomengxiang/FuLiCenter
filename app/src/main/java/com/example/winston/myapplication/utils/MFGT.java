cfca3b91e765b7a490db291856d91e1afbc37fad
wenxiaobo <279918814@qq.com>
2016-10-17 11:04:49 +0800

    工具类的添加

---
+++ app/src/main/java/com/example/winston/myapplication/utils/MFGT.java
@@ -0,0 +1,28 @@
+package com.example.winston.myapplication.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.example.winston.fulicenter.R;
+import com.example.winston.fulicenter.activity.MainActivity;
+
+/**
+ * Created by Winston on 2016/10/14.
+ * 辅助跳转
+ */
+    public class MFGT {
+        public static void finish(Activity activity){
+            activity.finish();
+            activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+        }
+        public static void gotoMainActivity(Activity context){
+            startActivity(context, MainActivity.class);
+        }
+        public static void startActivity(Activity context,Class<?> cls){
+            Intent intent = new Intent();
+            intent.setClass(context,cls);
+            context.startActivity(intent);
+            context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+        }
+    }
+
