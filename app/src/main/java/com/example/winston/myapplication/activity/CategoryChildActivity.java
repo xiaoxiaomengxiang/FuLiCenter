22dc2bfdc4e83c8a0bae0ced91503d5153681332
wenxiaobo <279918814@qq.com>
2016-10-20 23:06:52 +0800

    分类二级页面的数据显示

---
+++ app/src/main/java/com/example/winston/myapplication/activity/CategoryChildActivity.java
@@ -0,0 +1,159 @@
+package com.example.winston.myapplication.activity;
+
+
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.winston.myapplication.I;
+import com.example.winston.myapplication.R;
+import com.example.winston.myapplication.adapter.GoodsAdapter;
+import com.example.winston.myapplication.bean.NewGoodsBean;
+import com.example.winston.myapplication.net.NetDao;
+import com.example.winston.myapplication.net.OkHttpUtils;
+import com.example.winston.myapplication.utils.CommonUtils;
+import com.example.winston.myapplication.utils.ConvertUtils;
+import com.example.winston.myapplication.utils.L;
+import com.example.winston.myapplication.utils.MFGT;
+import com.example.winston.myapplication.view.SpaceItemDecoration;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Winston on 2016/10/20.
+ */
+
+public class CategoryChildActivity extends BaseActivity {
+    @BindView(R.id.tv_refresh)
+    TextView mTvRefresh;
+    @BindView(R.id.rv)
+    RecyclerView rv;
+    @BindView(R.id.srl)
+    SwipeRefreshLayout mSrl;
+    CategoryChildActivity mContext;
+    GoodsAdapter mAdapter;
+    ArrayList<NewGoodsBean> mList;
+    int pageId = 1;
+    GridLayoutManager glm;
+    int catId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_category_child);
+        ButterKnife.bind(this);
+        mContext = this;
+        mList = new ArrayList<>();
+        mAdapter = new GoodsAdapter(mContext, mList);
+        catId = getIntent().getIntExtra(I.CategoryChild.CAT_ID, 0);
+        if (catId == 0) {
+            finish();
+        }
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initView() {
+        mSrl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_yellow)
+        );
+        glm = new GridLayoutManager(mContext, I.COLUM_NUM);
+        rv.setLayoutManager(glm);
+        rv.setHasFixedSize(true);
+        rv.setAdapter(mAdapter);
+        rv.addItemDecoration(new SpaceItemDecoration(12));
+    }
+
+    @Override
+    protected void setListener() {
+        setPullUpListener();
+        setPullDownListener();
+    }
+
+    private void setPullDownListener() {
+        mSrl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSrl.setRefreshing(true);
+                mTvRefresh.setVisibility(View.VISIBLE);
+                pageId = 1;
+                downloadCategoryGoods(I.ACTION_PULL_DOWN);
+            }
+        });
+    }
+
+    private void downloadCategoryGoods(final int action) {
+        NetDao.downloadCategoryGoods(mContext,catId, pageId, new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
+            @Override
+            public void onSuccess(NewGoodsBean[] result) {
+                mSrl.setRefreshing(false);
+                mTvRefresh.setVisibility(View.GONE);
+                mAdapter.setMore(true);
+                L.e("result="+result);
+                if(result!=null && result.length>0){
+                    ArrayList<NewGoodsBean> list = ConvertUtils.array2List(result);
+                    if(action==I.ACTION_DOWNLOAD || action == I.ACTION_PULL_DOWN) {
+                        mAdapter.initData(list);
+                    }else{
+                        mAdapter.addData(list);
+                    }
+                    if(list.size()<I.PAGE_SIZE_DEFAULT){
+                        mAdapter.setMore(false);
+                    }
+                }else{
+                    mAdapter.setMore(false);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                mSrl.setRefreshing(false);
+                mTvRefresh.setVisibility(View.GONE);
+                mAdapter.setMore(false);
+                CommonUtils.showShortToast(error);
+                L.e("error:"+error);
+            }
+        });
+    }
+
+    private void setPullUpListener() {
+        rv.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int lastPosition = glm.findLastVisibleItemPosition();
+                if(newState == RecyclerView.SCROLL_STATE_IDLE
+                        && lastPosition == mAdapter.getItemCount()-1
+                        && mAdapter.isMore()){
+                    pageId++;
+                    downloadCategoryGoods(I.ACTION_PULL_UP);
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int firstPosition = glm.findFirstVisibleItemPosition();
+                mSrl.setEnabled(firstPosition==0);
+            }
+        });
+    }
+
+    @Override
+    protected void initData() {
+        downloadCategoryGoods(I.ACTION_DOWNLOAD);
+    }
+
+    @OnClick(R.id.backClickArea)
+    public void onClick() {
+        MFGT.finish(this);
+    }
+}
