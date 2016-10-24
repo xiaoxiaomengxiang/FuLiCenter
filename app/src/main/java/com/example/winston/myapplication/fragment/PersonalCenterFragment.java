dfa8c7970a46c6b29bad8ec99f4fb73ddd05841f
wenxiaobo <279918814@qq.com>
2016-10-24 20:20:19 +0800

    添加个人中心的用户数据显示

--- app/src/main/java/com/example/winston/myapplication/fragment/PersonalCenterFragment.java
+++ app/src/main/java/com/example/winston/myapplication/fragment/PersonalCenterFragment.java
@@ -5,18 +5,40 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.winston.myapplication.FuLiCenterApplication;
 import com.example.winston.myapplication.R;
+import com.example.winston.myapplication.activity.MainActivity;
+import com.example.winston.myapplication.bean.User;
+import com.example.winston.myapplication.utils.ImageLoader;
+import com.example.winston.myapplication.utils.L;
+import com.example.winston.myapplication.utils.MFGT;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Winston on 2016/10/24.
  */
 
 public class PersonalCenterFragment extends BaseFragment {
+    private static final String TAG = PersonalCenterFragment.class.getSimpleName();
+    @BindView(R.id.iv_user_avatar)
+    ImageView mIvUserAvatar;
+    @BindView(R.id.tv_user_name)
+    TextView mTvUserName;
+
+    MainActivity mContext;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.fragment_personal_center,container,false);
+        View layout = inflater.inflate(R.layout.fragment_personal_center, container, false);
+        ButterKnife.bind(this, layout);
+        mContext = (MainActivity) getActivity();
         super.onCreateView(inflater, container, savedInstanceState);
         return layout;
     }
@@ -28,11 +50,22 @@ public class PersonalCenterFragment extends BaseFragment {
 
     @Override
     protected void initData() {
-
+        User user = FuLiCenterApplication.getUser();
+        L.e(TAG,"user="+user);
+        if(user==null){
+            MFGT.gotoLogin(mContext);
+        }else{
+            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user),mContext,mIvUserAvatar);
+            mTvUserName.setText(user.getMuserNick());
+        }
     }
 
     @Override
     protected void setListener() {
 
+    }
+
+    @OnClick(R.id.tv_center_settings)
+    public void onClick() {
     }
 }
