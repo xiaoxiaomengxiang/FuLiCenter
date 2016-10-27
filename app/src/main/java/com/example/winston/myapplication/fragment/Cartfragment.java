7bec61899f21cb06f3e5f0be385580edcc260165
wenxiaobo <279918814@qq.com>
2016-10-28 00:11:00 +0800

    修复内存溢出的bug,sumPrice与setCartLayout互相调用死循环了

---
+++ app/src/main/java/com/example/winston/myapplication/fragment/Cartfragment.java
@@ -0,0 +1,196 @@
+package com.example.winston.myapplication.fragment;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.winston.myapplication.FuLiCenterApplication;
+import com.example.winston.myapplication.I;
+import com.example.winston.myapplication.R;
+import com.example.winston.myapplication.activity.MainActivity;
+import com.example.winston.myapplication.adapter.CartAdapter;
+import com.example.winston.myapplication.bean.CartBean;
+import com.example.winston.myapplication.bean.User;
+import com.example.winston.myapplication.net.NetDao;
+import com.example.winston.myapplication.net.OkHttpUtils;
+import com.example.winston.myapplication.utils.CommonUtils;
+import com.example.winston.myapplication.utils.L;
+import com.example.winston.myapplication.utils.ResultUtils;
+import com.example.winston.myapplication.view.SpaceItemDecoration;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class CartFragment extends BaseFragment {
+    private static final String TAG = CartFragment.class.getSimpleName();
+    @BindView(R.id.tv_refresh)
+    TextView mTvRefresh;
+    @BindView(R.id.rv)
+    RecyclerView mRv;
+    @BindView(R.id.srl)
+    SwipeRefreshLayout mSrl;
+    LinearLayoutManager llm;
+    MainActivity mContext;
+    CartAdapter mAdapter;
+    ArrayList<CartBean> mList;
+    @BindView(R.id.tv_cart_sum_price)
+    TextView mTvCartSumPrice;
+    @BindView(R.id.tv_cart_save_price)
+    TextView mTvCartSavePrice;
+    @BindView(R.id.layout_cart)
+    RelativeLayout mLayoutCart;
+    @BindView(R.id.tv_nothing)
+    TextView mTvNothing;
+
+    updateCartReceiver mReceiver;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_cart, container, false);
+        ButterKnife.bind(this, layout);
+        mContext = (MainActivity) getContext();
+        mList = new ArrayList<>();
+        mAdapter = new CartAdapter(mContext, mList);
+        super.onCreateView(inflater, container, savedInstanceState);
+        return layout;
+    }
+
+    @Override
+    protected void setListener() {
+        setPullDownListener();
+        IntentFilter filter = new IntentFilter(I.BROADCAST_UPDATA_CART);
+        mReceiver = new updateCartReceiver();
+        mContext.registerReceiver(mReceiver,filter);
+    }
+
+    private void setPullDownListener() {
+        mSrl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSrl.setRefreshing(true);
+                mTvRefresh.setVisibility(View.VISIBLE);
+                downloadCart();
+            }
+        });
+    }
+
+    @Override
+    protected void initData() {
+        downloadCart();
+    }
+
+    private void downloadCart() {
+        User user = FuLiCenterApplication.getUser();
+        if (user != null) {
+            NetDao.downloadCart(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<String>() {
+                @Override
+                public void onSuccess(String s) {
+                    ArrayList<CartBean> list = ResultUtils.getCartFromJson(s);
+                    L.e(TAG, "result=" + list);
+                    mSrl.setRefreshing(false);
+                    mTvRefresh.setVisibility(View.GONE);
+                    if (list != null && list.size() > 0) {
+                        mList.clear();
+                        mList.addAll(list);
+                        mAdapter.initData(mList);
+                        setCartLayout(true);
+                    }else{
+                        setCartLayout(false);
+                    }
+                }
+
+                @Override
+                public void onError(String error) {
+                    setCartLayout(false);
+                    mSrl.setRefreshing(false);
+                    mTvRefresh.setVisibility(View.GONE);
+                    CommonUtils.showShortToast(error);
+                    L.e("error:" + error);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void initView() {
+        mSrl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_yellow)
+        );
+        llm = new LinearLayoutManager(mContext);
+        mRv.setLayoutManager(llm);
+        mRv.setHasFixedSize(true);
+        mRv.setAdapter(mAdapter);
+        mRv.addItemDecoration(new SpaceItemDecoration(12));
+        setCartLayout(false);
+    }
+
+    private void setCartLayout(boolean hasCart) {
+        mLayoutCart.setVisibility(hasCart?View.VISIBLE:View.GONE);
+        mTvNothing.setVisibility(hasCart?View.GONE:View.VISIBLE);
+        mRv.setVisibility(hasCart?View.VISIBLE:View.GONE);
+        sumPrice();
+    }
+
+    @OnClick(R.id.tv_cart_buy)
+    public void onClick() {
+    }
+
+    private void sumPrice(){
+        int sumPrice = 0;
+        int rankPrice = 0;
+        if(mList!=null && mList.size()>0){
+            for (CartBean c:mList){
+                if(c.isChecked()){
+                    sumPrice += getPrice(c.getGoods().getCurrencyPrice())*c.getCount();
+                    rankPrice += getPrice(c.getGoods().getRankPrice())*c.getCount();
+                }
+            }
+            mTvCartSumPrice.setText("合计:￥"+Double.valueOf(rankPrice));
+            mTvCartSavePrice.setText("节省:￥"+Double.valueOf(sumPrice-rankPrice));
+
+        }else{
+            setCartLayout(false);
+            mTvCartSumPrice.setText("合计:￥0");
+            mTvCartSavePrice.setText("节省:￥0");
+        }
+    }
+    private int getPrice(String price){
+        price = price.substring(price.indexOf("￥")+1);
+        return Integer.valueOf(price);
+    }
+    class updateCartReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            L.e(TAG,"updateCartReceiver...");
+            sumPrice();
+            setCartLayout(mList!=null&&mList.size()>0);
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mReceiver!=null){
+            mContext.unregisterReceiver(mReceiver);
+        }
+    }
+}
